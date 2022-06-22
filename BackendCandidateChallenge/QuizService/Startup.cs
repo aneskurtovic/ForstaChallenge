acb2ffdd9e3e4b167dc28f2edc5effdc15b0abd2
anes.kurtovic <anes.kurtovic@gmail.com>
2022-06-22 18:55:11 +0200

    [anes] - added swagger, fixed failing tests temporary

--- BackendCandidateChallenge/QuizService/Startup.cs
+++ BackendCandidateChallenge/QuizService/Startup.cs
@@ -26,6 +26,7 @@ public class Startup
         services.AddMvc();
         services.AddSingleton(InitializeDb());
         services.AddControllers();
+        services.AddSwaggerGen();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,8 @@ public class Startup
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
         app.UseRouting();
         app.UseEndpoints(endpoints =>
